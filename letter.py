6be96aaae128a45c097e81381135e914c995fb29
Luzarait1 <111089098+Luzarait1@users.noreply.github.com>
2023-01-03 11:19:23 -0500

    Add files via upload

---
+++ letter.py
@@ -0,0 +1,10 @@
+# 1. Today's date.
+print("Todays date is: January 3, 2023.\nI am currently living in Colombia, Medellin")
+# 2. How you are feeling right now.
+print("I am feeling motivated and a little bit excited to learn")
+# 3. What you want to accomplish during this course.
+print("In this course i want to learn and solidify my knowledge of python")
+# 4. A little message to your older, wiser, and programmer self.
+print("All you have to do is continue to try, try your best, never give up and try even though it may be hard")
+# 5. Your favorite emoji to spice things up!
+print("My favorite emoji is 😊")
