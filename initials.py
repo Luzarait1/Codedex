a2cda536eab74560176b7035d5e9af74ff65556b
Luzarait1 <111089098+Luzarait1@users.noreply.github.com>
2023-01-03 11:07:09 -0500

    Add files via upload

---
+++ initials.py
@@ -0,0 +1,7 @@
+#A fun fact about myself is that i like to read books
+print("L              C")
+print("L            C    C")
+print("L            C")
+print("L            C")
+print("L            C    C")
+print("L  L  L        C")
