7658da0a6210f0b940fa488d2e098dddd41272cf
Luzarait1 <111089098+Luzarait1@users.noreply.github.com>
2023-01-04 17:58:36 -0500

    Add files via upload

---
+++ bmi.py
@@ -0,0 +1,9 @@
+
+#Calculates my own Body Mass Index
+
+mass = 51
+height = 1.62
+bmi = mass/(height**2)
+
+#print(bmi)
+print("My current BMI is: ", round(bmi, 2))
---
+++ temperature.py
@@ -0,0 +1,5 @@
+
+#the code converts fahrenheit to celsius
+F = 55
+celsius = round(((F - 32)/1.8), 2)
+print("The temperature of Brooklyn, NY in celsius is: ", celsius)
